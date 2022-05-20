aee091e698f458e3973e7ecd03ee4df24dfc8dcd
WinningPiano33 <milthon2012ac@hotmail.com>
2022-05-20 02:18:46 -0500

    se integro la lampara y se genero el APK

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
+import { Flashlight } from '@ionic-native/flashlight/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+ 
+  // booleano para indicar si se muestra o no la linterna
+  public active: boolean;
+ 
+  // inyectamos flashlight
+  constructor(
+    private flashlight: Flashlight
+  ) {
+    this.active = false;
+  }
+ 
+  flash(){
+    this.active = !this.active;
+ 
+    // si esta activo, encendemos la linterna, sino la apagamos
+    if(this.active){
+      this.flashlight.switchOn();
+    }else{
+      this.flashlight.switchOff();
+    }
+ 
+  }
+ 
 }
+
